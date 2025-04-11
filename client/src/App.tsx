8dc3ad4dee976020e4efa9859b22c8533d562848
hieucaotrantrong <hieu98861@donga.edu.vn>
2025-04-11 23:49:55 +0700

    display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import Carousel from './components/Carousel';
 import Header from './components/Header';
+import Footers from './components/Footers';
 function App() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <Carousel />
+      <Footers />
+    </>
+  );
 }
 
-export default App
+export default App;
