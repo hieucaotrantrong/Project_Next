1c600ffe8f1aa2a404640402e2eeb0de2bc5f44b
hieucaotrantrong <hieu98861@donga.edu.vn>
2025-07-21 17:23:14 +0700

    data

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    /*------------------------------------------
-    *Connect db used MysqlWordbend
-    -------------------------------------------*/
+//     /*------------------------------------------
+//     *Connect db used MysqlWordbend
+// //     -------------------------------------------*/
     host: 'localhost',
     user: 'root',
     password: 'hieu@1010',
@@ -14,3 +14,17 @@ const pool = mysql.createPool({
 });
 
 export default pool;
+ /*------------------------------------------
+    Docker
+    -------------------------------------------*/
+    // const pool = mysql.createPool({
+    //     host: process.env.DB_HOST || 'localhost',
+    //     user: process.env.DB_USER || 'root',
+    //     password: process.env.DB_PASSWORD || 'hieu@1010',
+    //     database: process.env.DB_NAME || 'clothes_db',
+    //     waitForConnections: true,
+    //     connectionLimit: 10,
+    //     queueLimit: 0
+    // });
+    
+    // export default pool;
